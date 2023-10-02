4d890745fde8f7fc0b70563817353393ecce2b5d
Benrobo <alumonabenaiah71@gmail.com>
2023-10-02 02:42:21 +0100

    added validation method

--- test.js
+++ test.js
@@ -6,6 +6,13 @@ function initializeAssistant() {
   assist.loadAssets("./lib/assets/assistant.gif").highlight({
     steps: [
       {
+        element: "#button12",
+        info: {
+          title: "Some title here",
+          //   description: "some description",
+        },
+      },
+      {
         element: "#button1",
         info: {
           title: "Some title here",
