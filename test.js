3c7e52de67f753abd1908220697739bccdecede2
Benrobo <alumonabenaiah71@gmail.com>
2023-10-02 02:25:52 +0100

    updated

--- test.js
+++ test.js
@@ -3,7 +3,17 @@ import PageAssistant from "./lib/assistant";
 const assist = new PageAssistant();
 
 function initializeAssistant() {
-  assist.loadAssets("./lib/assets/assistant.gif").hey();
+  assist.loadAssets("./lib/assets/assistant.gif").highlight({
+    steps: [
+      {
+        element: "#button1",
+        info: {
+          title: "Some title here",
+          description: "some description",
+        },
+      },
+    ],
+  });
 }
 
 initializeAssistant();
